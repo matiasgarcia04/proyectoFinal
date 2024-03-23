596be7860bb40bf47ba5641941f4a3ce9ebbd94d
matiasgarcia <matiasgustavogarcia.04@gmail.com>
2024-03-23 19:43:41 -0300

    actualizada preentrega

--- src/controllers/cartViews.routers.controller.js
+++ src/controllers/cartViews.routers.controller.js
@@ -1,7 +1,12 @@
 import CartManagerDB from "../dao/CartsProdManagerDB.js";
+import purchaseManagerDB from "../dao/purchaseManagerDB.js";
+import ProdManagerDB from "../dao/ProdManagerDB.js";
+
 
 
 const cartDB= new CartManagerDB();
+const probDB = new ProdManagerDB();
+const ticketDB = new purchaseManagerDB();
 
 class cartctrl {
     getCart = async (req,res)=>{
@@ -15,6 +20,82 @@ class cartctrl {
                console.log(error)
             }
     }
+    purchase = async (req,res)=>{
+        try {
+                const {pid,cid}= req.params;
+                const cart = await cartDB.getByID(cid)
+                if (!cart) {
+                    return res.status(401).json({
+                        status: 'error',
+                        message: 'Cart not found'
+                    });
+                    }
+    
+                const productsNotPurchased = [];
+                let totalPrice=0
+    
+                // comprobar stock, si no hay suficiente stock, lo manda al Array, si hay continua y resta cantidad del stock
+                for (const item of cart.products) {
+                            const product = item.product;
+                            
+                            if (product.stock < item.quantity) {
+                                productsNotPurchased.push(product._id);
+                            }else {
+                                // Si hay suficiente stock, restamos la cantidad comprada del stock del producto
+                                
+                                    
+                                    product.stock -= item.quantity;
+                                    await product.save();
+                                    
+                                    totalPrice += product.price * item.quantity;
+                            };
+                        }
+                        
+        // generar ticker
+                const ticket = await ticketDB.create({
+                        code: Math.random().toString(30).substring(2),
+                        amount: totalPrice,
+                        // purchaser: req.user.email,
+                        purchaser: "email@email.com",
+                        });
+    
+        
+        for (const item of cart.products) {
+        const product = item.product;
+        cartDB.delete(cid);
+        if (!productsNotPurchased.includes(product._id)) {
+            product.stock -= item.quantity;
+            await product.save();
+        } 
+        }
+        for (const productId of productsNotPurchased) {
+        // Encuentra el producto correspondiente en la base de datos
+        const productToAdd = await probDB.getByID(productId);
+        if (productToAdd) {
+            // Agrega el producto al carrito con una cantidad predeterminada (por ejemplo, 1)
+            cart.products.push({
+                product: productToAdd,
+                quantity: 1,
+            });
+        }
+        }
+        await cart.save();
+    
+                // return res.status(200).json({ message: 'Compra exitosa' });
+                res.status(200).json({
+                    status: 'success',
+                    message: 'Purchase completed successfully',
+                    productsNotPurchased,
+                    ticket,
+                    
+                    });
+            } catch (error) {
+                console.error('Error al finalizar la compra:', error);
+            return res.status(500).json({ message: 'Error interno del servidor' });
+            }
+    }
+
+
 }
 
 
--- src/dao/CartsProdManagerDB.js
+++ src/dao/CartsProdManagerDB.js
@@ -26,11 +26,6 @@ class CartManagerDB {
       }
 
 
-      async add2({filter}) {
-        const cart = await cartModel.findByIdAndUpdate({filter});
-        return cart;
-      }
-
     async update(uid){
         return await cartModel.findByIdAndUpdate({_id: uid})
 
@@ -45,6 +40,36 @@ class CartManagerDB {
     async getByIDLean(uid){
       return await cartModel.findOne({_id: uid}).lean();
       }
+
+
+    // Delete api/carts/:cid/products/:pid
+    async deleteItem(cid, pid){
+      try {
+          return await this.Cart.findOneAndUpdate(
+              { _id: cid },
+              { $pull: { products: { product: pid } } },
+              { new: true }
+          )
+      } catch (error) {
+          return new Error('Error deleting product from cart'+error)
+      }
+  }
+    // vaciar carrito
+    async delete(cid){
+      try {
+          // console.log(cid)
+          return await cartModel.findOneAndUpdate(
+              { _id: cid },
+              { $set: { products: [] } },
+              { new: true }
+          )
+      } catch (error) {
+          return new Error('Error deleting cart'+ error)
+      }
+  }
+    
+
+
 }
 
 
