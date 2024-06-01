c9698e164df0f8bb9f10b49694e997b6ac1fc55d
matiasgarcia <matiasgustavogarcia.04@gmail.com>
2024-06-01 00:10:46 -0300

    arreglo de middleware isadmin

--- src/middleware/isAdmin.js
+++ src/middleware/isAdmin.js
@@ -1,11 +1,10 @@
 
 const isAdmin = (req, res, next) => {
-    if (req.session.user && req.session.user.role === 'ADMIN' || 'USER_PREMIUM') {
+    if (req.session.user && (req.session.user.role === 'ADMIN' || req.session.user.role === 'USER_PREMIUM')) {
         next();
     } else {
         res.redirect('/products');
     }
 };
-
 
 export default isAdmin
